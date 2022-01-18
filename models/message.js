39c62fc0b5ba0aecee8b6e38cfcaca2b17daac24
BSY-Development <bes.yamamoto@gmail.com>
2022-01-18 18:23:07 -0300

    Requisito 3

--- models/message.js
+++ models/message.js
@@ -1,13 +1,32 @@
-const formatMessage = (chatMessage, nickname) => {
-  const fullDate = new Date();
-  const formatDate = `${fullDate.getDate()}-${fullDate.getMonth() + 1}-${fullDate.getFullYear()}`;
-  const formatTime = `${fullDate.getHours()}:${fullDate.getMinutes()}:${fullDate.getSeconds()}`;
-  return `${formatDate} ${formatTime} - ${nickname}: ${chatMessage}`;
-};
+const moment = require('moment');
+const connection = require('./connection');
+
+const formatMessage = () => moment().format('DD-MM-YYYY HH:mm:ss');
 
 const removeSelectedUser = (id, list) => list.filter((item) => item.id !== id);
+
+const getAllMessages = async () => {
+  try {
+      const makeConnection = await connection();
+      const getAll = await makeConnection.collection('messages').find().toArray();
+      return getAll;
+  } catch (error) {
+      console.error(error);
+  }
+};
+
+const saveMessage = async (nickname, message, timestamp) => {
+  try {
+      const makeConnection = await connection();
+      await makeConnection.collection('messages').insertOne({ message, nickname, timestamp });
+  } catch (error) {
+      console.error(error);
+  }
+};
 
 module.exports = {
   formatMessage,
   removeSelectedUser,
+  getAllMessages,
+  saveMessage,
 };
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,23 +1,27 @@
-const { formatMessage, removeSelectedUser } = require('../models/message');
+const { 
+  formatMessage, removeSelectedUser, getAllMessages, saveMessage,
+} = require('../models/message');
 
 let userList = [];
 
-module.exports = (io) => io.on('connection', (socket) => {
+module.exports = (io) => io.on('connection', async (socket) => {
+  socket.emit('allMessages', await getAllMessages());
+
   socket.on('message', ({ chatMessage, nickname }) => {
-    const formattedMessage = formatMessage(chatMessage, nickname);
-    io.emit('message', formattedMessage);
+    saveMessage(nickname, chatMessage, formatMessage());
+    io.emit('message', `${formatMessage()} - ${nickname}: ${chatMessage}`);
   });
   socket.on('newUser', (nickname) => {
     userList.push({ id: socket.id, nickname });
     io.emit('newUser', userList);
   });
-  socket.on('changeNick', (newNickname) => {
+  socket.on('changeNick', (newNick) => {
     userList = removeSelectedUser(socket.id, userList);
-    userList.push({ id: socket.id, nickname: newNickname });
+    userList.push({ id: socket.id, nickname: newNick });
     io.emit('newUser', userList);
   });
   socket.on('disconnect', () => {
     userList = removeSelectedUser(socket.id, userList);
     io.emit('newUser', userList);
-});
+  });
 });

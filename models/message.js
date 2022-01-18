d129d096ec3b3a2b0de92063fad5527969a1b925
BSY-Development <bes.yamamoto@gmail.com>
2022-01-18 20:16:41 -0300

    Removido o moment para teste

--- models/message.js
+++ models/message.js
@@ -1,7 +1,13 @@
-const moment = require('moment');
+// const moment = require('moment');
 const connection = require('./connection');
 
-const formatMessage = () => moment().format('DD-MM-YYYY HH:mm:ss');
+// const formatMessage = () => moment().format('DD-MM-YYYY HH:mm:ss');
+const formatMessage = () => {
+  const fullDate = new Date();
+  const formatDate = `${fullDate.getDate()}-${fullDate.getMonth() + 1}-${fullDate.getFullYear()}`;
+  const formatTime = `${fullDate.getHours()}:${fullDate.getMinutes()}:${fullDate.getSeconds()}`;
+  return `${formatDate} ${formatTime}`;
+};
 
 const removeSelectedUser = (id, list) => list.filter((item) => item.id !== id);
 

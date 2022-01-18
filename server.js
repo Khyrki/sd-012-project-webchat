63a6a108e021218977620e4b5d6179bc625634a7
Rodrigo Merlone <alestori7@gmail.com>
2022-01-18 14:26:41 -0300

    setup express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 // Faça seu código aqui
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const { PORT } = process.env;
+
+app.listen(PORT || 3000, () => console.log(`Listening on port ${PORT}`));
+
+app.use(express.json());

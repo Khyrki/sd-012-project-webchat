e2d4ae2bbc7d0f6e63af094e42e7b31a1b6760c7
Gonzagadavid <gonzagadaviddev@gmail.com>
2022-01-18 18:28:21 -0300

    Criado um back-end para conexão simultaneamente de clientes e troca de mensagens em chat público

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+require('dotenv').config();
+
+const { PORT = 3000 } = process.env;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  } });
+
+  app.use(express.static(`${__dirname}/public`));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+require('./socket')(io);
+
+http.listen(PORT, () => console.log(`server rodando na porta ${PORT}`));

53f8ddc688374f7fe92d4e8d28ebabbd1c354578
BSY-Development <bes.yamamoto@gmail.com>
2022-01-18 17:15:10 -0300

    Requisito 2

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,8 +1,20 @@
 const { formatMessage } = require('../models/message');
 
+let userList = [];
+
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', ({ chatMessage, nickname }) => {
     const formattedMessage = formatMessage(chatMessage, nickname);
     io.emit('message', formattedMessage);
+  });
+  socket.on('newUser', (nickname) => {
+    userList.push({ id: socket.id, nickname });
+    io.emit('newUser', userList);
+  });
+  socket.on('changeNick', ({ oldNickname, newNickname }) => {
+    const removeOld = userList.filter((item) => item.nickname !== oldNickname);
+    removeOld.push({ id: socket.id, nickname: newNickname });
+    userList = removeOld;
+    io.emit('newUser', userList);
   });
 });

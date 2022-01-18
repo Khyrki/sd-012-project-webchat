152d3ec87c5588b379d3f238bee74c69eff61a79
Gabriel Takaki <gabitj@icloud.com>
2022-01-18 17:43:08 -0300

    corrige formato do horario req 1

--- sockets/client.js
+++ sockets/client.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', ({ chatMessage, nickname }) => {
-    const date = moment().format('DD/MM/YYYY HH:mm:ss');
+    const date = moment().format('DD-MM-YYYY HH:mm:ss A');
     io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
   });
 });

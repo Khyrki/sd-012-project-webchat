a1fe7fd94065c3b59d3b46a3061210f5b63961f7
Gabriel Viana <g.viana87@hotmail.com>
2022-01-18 16:56:35 -0300

    iniciando o projeto web chat

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-// Faça seu código aqui
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(htts, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'], 
+  }
+});
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`))

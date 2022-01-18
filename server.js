01e0647bbcf1f877030b90bb759de0f0fc79985a
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2022-01-18 13:45:22 -0300

    fazendo configuracao inicial do server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+
+const PORT = proces.env.PORT || 3000;
+
+const app = express();
+const httpServer = http.createServer(app);
+app.use(cors());
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server conectado na porta${PORT}`);
+});

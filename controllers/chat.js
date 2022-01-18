f82bda4167acd606cce40725ff5c24ba2f282db0
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2022-01-18 15:08:15 -0300

    fazendo a estrutura basica do projeto

---
+++ controllers/chat.js
@@ -0,0 +1,3 @@
+module.exports = (_req, res) => {
+  res.render('chat/chat')
+};
--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+const chatController = require('./controllers/chat');
+require('dotenv').config();
 
-const PORT = proces.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const httpServer = http.createServer(app);
 app.use(cors());
 
-const io = require('socket.io')(http, {
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const io = require('socket.io')(httpServer, {
   cors: {
     origin: `http://localhost:${PORT}`,
     methods: ['GET', 'POST'],
   },
 });
 
+app.get('/', chatController);
+
 httpServer.listen(PORT, () => {
-  console.log(`Server conectado na porta${PORT}`);
+  console.log(`Server conectado na porta ${PORT}`);
 });

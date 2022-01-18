eb7949f015c86aaa359d7f8df052b3ae1eb08a8b
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2022-01-18 17:21:23 -0300

    Iniciando projeto

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+const { Server } = require('socket.io');
+
+const { PORT = 3001 } = process.env;
+
+const io = new Server(http, { cors: { origin: 'http://localhost:3000', methods: ['GET'] } });
+
+io.listen(PORT, console.log(`Listening on port ${PORT}`));

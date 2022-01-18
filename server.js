29948b8a6d7d033ae4528ae7b0d9d79999ccf901
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-01-18 15:52:34 -0300

    criando o controller

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/front-end/index.html');
+  res.sendFile(`${__dirname}/front-end/index.html`);
 });
 
 io.on('connection', (socket) => {
     socket.on('message', ({ nickname, chatMessage }) => {
-      let  now = new Date
+      const now = new Date();
       const returnMessage = `${now.getDate()}-${now.getMonth()}-${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} - ${nickname}: ${chatMessage}`;
       io.emit('message', returnMessage);
     });

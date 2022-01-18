3a9367e373760a80dc00c63cddbf106c5997aa33
Michel Pereira <michelbecauro@gmail.com>
2022-01-18 16:12:57 -0300

    feat: Set express server and Endpoint

--- server.js
+++ server.js
@@ -1,1 +1,47 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000/',
+    methods: ['GET', 'POST'],
+  },
+});
+const messagesModel = require('./models/messagesModel');
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(`${__dirname}/frontend`));
+
+const PORT = 3000;
+
+app.use(cors());
+
+require('./backend/socket')(io);
+
+app.get('/', async (_req, res) => {
+  const messages = await messagesModel.getAll();
+
+  res.render('chat', { messages });
+});
+
+app.post('/', async (req, res) => {
+  const { time, nickname, message } = req.body;
+
+  try {
+    await messagesModel.create({ time, nickname, message });
+    res.status(201).send('Insertion successfully');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/ping', (_req, res) => res.send('PONG!'));
+
+http.listen(PORT, () => console.log(`Running Wechat in ${PORT} port`));

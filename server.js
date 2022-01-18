f2577e028439ad440d72d7ecd3a981cc87a6799c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-01-17 21:38:27 -0300

    cria server.js e pre-configura sockets/chat.js

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-// Faça seu código aqui
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+app.use(express.static(`${__dirname}/views`));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+require('./sockets/chat')(io);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+server.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));

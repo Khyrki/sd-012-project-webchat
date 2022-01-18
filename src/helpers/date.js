50281e106237adfad57d9c2aa57acdf9e3bfef42
Roberval Filho <rslfilho@gmail.com>
2022-01-18 15:31:31 -0300

    Corrigindo captura do minuto

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -7,7 +7,7 @@ const formateDate = (date) => {
 const formateTime = (date) => {
   const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
   const minute = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-  const second = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  const second = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
   return `${hour}:${minute}:${second}`;
 };
 

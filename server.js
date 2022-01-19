3292ca65fe1bc8dbbd926f39ce47a3034d372861
thales <thalesdaniel2013@gmail.com>
2022-01-19 16:41:10 -0300

    corrigido problema com npm

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado: ${socket.id}`);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static(`${__dirname}/public`));
+
+http.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});

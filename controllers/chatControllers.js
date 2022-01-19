bb4802ccae24dc3401a409a131a24f31d864c891
Jossany Moura <mjossany@gmail.com>
2022-01-19 11:17:10 -0300

    Implementa root no server.js

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -3,7 +3,7 @@ const getAll = require('../models/getAllMessages.js');
 const getAllMessages = async (_req, res) => {
   const messages = await getAll();
 
-  res.status(200).render('chat', messages);
+  res.status(200).render('views/chat', messages);
 };
 
 module.exports = getAllMessages;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 const messagesSocket = require('./sockets/messages');
 const usersSocket = require('./sockets/users');
+const root = require('./routes/root');
 
 const app = express();
 const httpServer = createServer(app);
@@ -18,11 +18,7 @@ app.set('view engine', 'ejs');
 
 app.set('views', './views');
 
-app.use(express.static(path.resolve(__dirname, '/public')));
-
-app.get('/', (_req, res) => {
-  res.status(200).render('chat');
-});
+app.use('/', root);
 
 messagesSocket(io);
 usersSocket(io);

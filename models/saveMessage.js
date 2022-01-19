52ebfa14dd787d66d768b53a9e72dd994e9a8f8d
Jossany Moura <mjossany@gmail.com>
2022-01-19 16:26:26 -0300

    Corrige model de salvar mensagem

--- models/saveMessage.js
+++ models/saveMessage.js
@@ -1,13 +1,13 @@
 const connection = require('./connection');
 
-module.exports = async ({ chatMessage, nickname, treatedDate }) => {
+module.exports = async ({ chatMessage, nickName, treatedDate }) => {
   const db = await connection();
   const collection = await db.collection('messages');
-  const { insertedId } = await collection.insertOne({ chatMessage, nickname, treatedDate });
+  const { insertedId } = await collection.insertOne({ chatMessage, nickName, treatedDate });
   return {
     id: insertedId,
     chatMessage,
-    nickname,
+    nickName,
     treatedDate,
   };
 };

b6f6c1bc6e2baf65e6ac7908be9c1f817610340b
Gabriel Takaki <gabitj@icloud.com>
2022-01-19 16:09:15 -0300

    requsito 4

--- sockets/client.js
+++ sockets/client.js
@@ -1,14 +1,33 @@
 const moment = require('moment');
 const Message = require('../models/messages');
 
+const clients = [];
+
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', async ({ chatMessage, nickname }) => {
     const date = moment().format('DD-MM-YYYY HH:MM:SS A');
-    await Message.create({
-      message: chatMessage,
-      nickname,
-      timestamp: date,
-    });
+    await Message.create({ message: chatMessage, nickname, timestamp: date });
     io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
   });
+
+  socket.on('updateNickname', (nickname) => {
+    clients.splice(clients.indexOf(clients.find((client) => client.id === socket.id)), 1);
+    clients.push({ id: socket.id, nickname });
+    io.emit('online', clients);
+  });
+
+  socket.on('create', (nickname) => {
+    clients.push({ id: socket.id, nickname });
+    io.emit('online', clients);
+  });
+
+  socket.on('disconnect', () => {
+    // const client = clients.findIndex((user) => user.id === socket.id);
+    // clients.splice(client, 1);
+    // io.emit('online', clients);
+    clients.splice(clients.indexOf(clients.find((client) => client.id === socket.id)), 1);
+    io.emit('online', clients);
+  });
 });
+
+// io socket online
--- views/chat.js
+++ views/chat.js
@@ -1,16 +1,21 @@
 const socket = window.io();
 
+const dataId = 'data-testid';
+
 // const formMessage = document.querySelector('.form-message');
 const inputMessage = document.querySelector('.message-box');
 const sendButton = document.querySelector('.send-button');
-
 const nicknameInput = document.querySelector('.nickname-box');
 const nicknameButton = document.querySelector('.nickname-button');
-
 const onlineUser = document.querySelector('.online-user');
+const messageUl = document.querySelector('.messages');
+const onlineUsers = document.querySelector('.online-users');
 
 const randomName = Math.random().toString(10).substr(2, 16);
-onlineUser.innerHTML = sessionStorage.getItem('nickname') || randomName;
+
+const sessionUser = sessionStorage.getItem('nickname') || randomName;
+onlineUser.innerHTML = sessionUser;
+socket.emit('create', onlineUser.innerHTML);
 
 sendButton.addEventListener('click', () => {
   socket.emit('message', { chatMessage: inputMessage.value, nickname: onlineUser.innerHTML });
@@ -23,14 +28,31 @@ nicknameButton.addEventListener('click', () => {
   nicknameInput.value = '';
   onlineUser.innerHTML = newNickname;
   sessionStorage.setItem('nickname', newNickname);
+  socket.emit('updateNickname', newNickname);
 });
 
-const messageUl = document.querySelector('.messages');
+socket.on('online', (list) => {
+  // onlineUsers.innerHTML = '';
+  // const givenUser = sessionStorage.getItem('nickname');
+  // const li = document.createElement('li');
+  // li.setAttribute(dataId, 'online-user');
+  // li.innerText = givenUser;
+  // onlineUsers.appendChild(li);
+
+  list.forEach((user) => {
+    // Basiado em https://github.com/tryber/sd-012-project-webchat/pull/1/commits/c3c35f5245126f97a31f415a96a4c48c4898c42f
+    if (user.nickname === sessionUser) console.log('igual');
+    const liEl = document.createElement('li');
+    liEl.setAttribute(dataId, 'online-user');
+    liEl.innerText = user.nickname;
+    onlineUsers.appendChild(liEl);
+  });
+});
 
 function messageCreation(message) {
   const messageLi = document.createElement('li');
   messageLi.classList.add('message');
-  messageLi.setAttribute('data-testid', 'message');
+  messageLi.setAttribute(dataId, 'message');
   messageLi.innerText = message;
 
   messageUl.appendChild(messageLi);
@@ -38,4 +60,4 @@ function messageCreation(message) {
 
 socket.on('message', (message) => messageCreation(message));
 
-// window.onbeforeunload = (_e) => socket.disconnect();
+window.onbeforeunload = (_e) => socket.disconnect();

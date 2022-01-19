a054a24946ce81eb4f6ac7c8c9d42dbf4051a9db
julio11lp <julio11lp@hotmail.com>
2022-01-19 09:50:17 -0300

    fix: refazendo o projeto, requisito 1 aprovado no lint e test

--- public/js/script.js
+++ public/js/script.js
@@ -1,79 +1,77 @@
-const socket = window.io();
+// const socket = window.io();
 
-window.onbeforeunload = (_event) => {
-  socket.disconnect();
-};
+// window.onbeforeunload = (_event) => {
+//   socket.disconnect();
+// };
 
-const nicknameForm = document.querySelector('#nicknameForm');
-const nicknameInput = document.querySelector('#nicknameInput');
-const nicknameList = document.querySelector('#nicknameList');
-const messageForm = document.querySelector('#messageForm');
-const messageInput = document.querySelector('#messageInput');
-const messageList = document.querySelector('#messageList');
+// const nicknameForm = document.querySelector('#nicknameForm');
+// const nicknameInput = document.querySelector('#nicknameInput');
+// const nicknameList = document.querySelector('#nicknameList');
+// const messageForm = document.querySelector('#messageForm');
+// const messageInput = document.querySelector('#messageInput');
+// const messageList = document.querySelector('#messageList');
 
-nicknameForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  socket.emit('changeNick', nicknameInput.value);
-  nicknameInput.value = '';
-  return false;
-});
+// nicknameForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   socket.emit('changeNick', nicknameInput.value);
+//   nicknameInput.value = '';
+//   return false;
+// });
 
-messageForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+// messageForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
 
-  const message = messageInput.value;
-  const nickname = document.querySelector('#userName').innerText;
+//   const message = messageInput.value;
+//   const nickname = document.querySelector('#userName').innerText;
 
-  const chatMessage = message;
+//   const chatMessage = message;
 
-  socket.emit('message', { chatMessage, nickname });
+//   socket.emit('message', { chatMessage, nickname });
 
-  messageInput.value = '';
-  return false;
-});
+//   messageInput.value = '';
+//   return false;
+// });
 
-const createNicknameList = (nickNameArray) => {
-  nicknameList.innerHTML = '';
-  nickNameArray.forEach(({ nickname }) => {
-    const listItem = document.createElement('li');
-    listItem.setAttribute('class', 'nickname');
-    listItem.setAttribute('data-testid', 'online-user');
+// const createNicknameList = (nickNameArray) => {
+//   nicknameList.innerHTML = '';
+//   nickNameArray.forEach(({ nickname }) => {
+//     const listItem = document.createElement('li');
+//     listItem.setAttribute('class', 'nickname');
+//     listItem.setAttribute('data-testid', 'online-user');
   
-    listItem.innerHTML = nickname;
+//     listItem.innerHTML = nickname;
   
-    nicknameList.appendChild(listItem);
-  });
-};
+//     nicknameList.appendChild(listItem);
+//   });
+// };
 
-const createMessage = (messagesArray) => {
-  while (messageList.firstChild) {
-    messageList.removeChild(messageList.firstChild);
-  }
-  messagesArray.forEach(({ chatMessage, nickname, timestamp }) => {
-    const message = `${timestamp} - ${nickname}: ${chatMessage}`;
-    const listItem = document.createElement('li');
-    listItem.setAttribute('data-testid', 'message');
-    listItem.innerText = message;
+// const createMessage = (messagesArray) => {
+//   messageList.innerHTML = '';
+//   messagesArray.forEach(({ chatMessage, nickname, timestamp }) => {
+//     const message = `${timestamp} - ${nickname}: ${chatMessage}`;
+//     const listItem = document.createElement('li');
+//     listItem.setAttribute('data-testid', 'message');
+//     listItem.innerText = message;
 
-    messageList.appendChild(listItem);
-  });
-};
+//     messageList.appendChild(listItem);
+//   });
+// };
 
-const defineActualUserNamer = (userName) => {
-  const messageLabel = document.querySelector('#userName');
+// const defineActualUserNamer = (userName) => {
+//   const messageLabel = document.querySelector('#userName');
 
-  messageLabel.innerText = userName;
-};
+//   messageLabel.innerText = userName;
+// };
 
-const createSystemMessage = (message) => {
-  const listItem = document.createElement('li');
+// const createSystemMessage = (message) => {
+//   const listItem = document.createElement('li');
 
-  listItem.innerText = message;
+//   listItem.innerText = message;
 
-  messageList.appendChild(listItem);
-};
+//   messageList.appendChild(listItem);
+// };
 
-socket.on('message', (messagesArray) => createMessage(messagesArray));
-socket.on('systemMessage', (systemMessage) => createSystemMessage(systemMessage));
-socket.on('userName', (userName) => defineActualUserNamer(userName));
-socket.on('nicknameList', (nickNameArray) => createNicknameList(nickNameArray));
+// socket.on('message', (messagesArray) => createMessage(messagesArray));
+// socket.on('systemMessage', (systemMessage) => createSystemMessage(systemMessage));
+// socket.on('userName', (userName) => defineActualUserNamer(userName));
+// socket.on('nicknameList', (nickNameArray) => createNicknameList(nickNameArray));
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,122 +1,126 @@
-const generator = require('nickname-generator');
+// const generator = require('nickname-generator');
 const { format } = require('date-fns');
 
-let users = [];
-const messagesArray = [];
+// let users = [];
+// let ioUsers = [];
+// const messagesArray = [];
 
-const nicknameGenerator = (id) => {
-  let nickname = generator.randomNickname({ locale: 'fr', separator: '-', suffixLength: 0 });
+// const nicknameGenerator = (id) => {
+//   let nickname = generator.randomNickname({ locale: 'fr', separator: '-', suffixLength: 0 });
 
-  while (nickname.length < 16) {
-    nickname = generator.randomNickname({ locale: 'fr', separator: '-', suffixLength: 0 });
-    if (nickname.length > 16) {
-      nickname = nickname.slice(0, 16);
-    }
-  }
+//   while (nickname.length < 16) {
+//     nickname = generator.randomNickname({ locale: 'fr', separator: '-', suffixLength: 0 });
+//     if (nickname.length > 16) {
+//       nickname = nickname.slice(0, 16);
+//     }
+//   }
 
-  users.unshift({
-    id,
-    nickname,
-  });
+//   users.unshift({
+//     id,
+//     nickname,
+//   });
 
-  const loginNotification = `Usuário ${nickname} se conectou.`;
+//   ioUsers.push({ id, nickname });
 
-  return {
-    loginNotification,
-    nickname,
-  };
-};
+//   const loginNotification = `Usuário ${nickname} se conectou.`;
 
-const disconnectUser = (socketId) => {
-  const { nickname } = users.find(({ id }) => id === socketId);
+//   return {
+//     loginNotification,
+//     nickname,
+//   };
+// };
 
-  users = users.filter(({ id }) => id !== socketId);
+// const disconnectUser = (socketId) => {
+//   const { nickname } = users.find(({ id }) => id === socketId);
 
-  const disconnect = `Usuário ${nickname} se desconectou`;
+//   users = users.filter(({ id }) => id !== socketId);
 
-  return disconnect;
-};
+//   const disconnect = `Usuário ${nickname} se desconectou`;
 
-const createMessage = (io, socket, id) => {
+//   return disconnect;
+// };
+
+const createMessage = (io, socket, _id) => {
   socket.on('message', ({ chatMessage, nickname }) => {
     const timestamp = format(new Date(), 'dd-MM-yyy HH:mm:ss');
-    // const message = `${timestamp} - ${nickname}: ${chatMessage}`;
-    messagesArray.push({ chatMessage, id, nickname, timestamp });
-    io.emit('message', messagesArray);
+    const message = `${timestamp} - ${nickname}: ${chatMessage}`;
+    // messagesArray.push({ chatMessage, id, nickname, timestamp });
+    io.emit('message', message);
   });
 };
 
-const disconnect = (io, socket, id) => {
-  socket.on('disconnect', () => {
-    // const disconnectNotification = disconnectUser(id);
-    disconnectUser(id);
-    // io.emit('systemMessage', disconnectNotification);
-    io.emit('nicknameList', users);
-  });
-};
+// const disconnect = (io, socket, id) => {
+//   socket.on('disconnect', () => {
+//     // const disconnectNotification = disconnectUser(id);
+//     disconnectUser(id);
+//     // io.emit('systemMessage', disconnectNotification);
+//     io.emit('nicknameList', users);
+//   });
+// };
 
-const updateMessageNickname = (id, newNickname) => {
-  // const indexArray = messagesArray.map((item, index) => {
-  //   if (item.id === id) return index;
-  // });
-  const indexArray = [];
-  messagesArray.forEach((item, index) => {
-    if (item.id === id) {
-      indexArray.push(index);
-    }
-  });
-  indexArray.forEach((index) => {
-    messagesArray[index].nickname = newNickname;
-  });
-};
+// const updateMessageNickname = (id, newNickname) => {
+//   // const indexArray = messagesArray.map((item, index) => {
+//   //   if (item.id === id) return index;
+//   // });
+//   const indexArray = [];
+//   messagesArray.forEach((item, index) => {
+//     if (item.id === id) {
+//       indexArray.push(index);
+//     }
+//   });
+//   indexArray.forEach((index) => {
+//     messagesArray[index].nickname = newNickname;
+//   });
+// };
 
-const updateNickname = (id, oldNickname, newNickname) => {
-  let indexOf = '';
-    users.find((item, index) => {
-      indexOf = index;
-      return item.id === id;
-    });
-    users[indexOf].nickname = newNickname;
-    const { nickname } = users[indexOf];
+// const updateNickname = (id, oldNickname, newNickname) => {
+//   let indexOf = '';
+//     users.find((item, index) => {
+//       indexOf = index;
+//       return item.id === id;
+//     });
+//     users[indexOf].nickname = newNickname;
+//     const { nickname } = users[indexOf];
 
-    updateMessageNickname(id, newNickname);
+//     updateMessageNickname(id, newNickname);
 
-    const ioNotification = `${oldNickname} alterou seu apelido para ${nickname}`;
-    const socketNotification = `Você alterou seu apelido para ${nickname}`;
+//     const ioNotification = `${oldNickname} alterou seu apelido para ${nickname}`;
+//     const socketNotification = `Você alterou seu apelido para ${nickname}`;
 
-    return {
-      nickname,
-      ioNotification,
-      socketNotification,
-    };
-};
+//     return {
+//       nickname,
+//       ioNotification,
+//       socketNotification,
+//     };
+// };
 
-const nicknameManager = (io, socket, id) => {
-  const { nickname: oldNickname } = users.find(({ id: socketId }) => socketId === id);
+// const nicknameManager = (io, socket, id) => {
+//   const { nickname: oldNickname } = users.find(({ id: socketId }) => socketId === id);
 
-  socket.on('changeNick', (nick) => {
-    const { nickname } = updateNickname(id, oldNickname, nick);
+//   socket.on('changeNick', (nick) => {
+//     const { nickname } = updateNickname(id, oldNickname, nick);
 
-    // socket.broadcast.emit('systemMessage', ioNotification);
-    io.emit('nicknameList', users);
-    io.emit('message', messagesArray);
-    // socket.emit('systemMessage', socketNotification);
-    socket.emit('userName', nickname);
-  });
-};
+//     // socket.broadcast.emit('systemMessage', ioNotification);
+//     io.emit('nicknameList', users);
+//     io.emit('message', messagesArray);
+//     // socket.emit('systemMessage', socketNotification);
+//     socket.emit('userName', nickname);
+//   });
+// };
 
 module.exports = (io) => io.on('connection', (socket) => {
   const { id } = socket;
   
-  const { nickname } = nicknameGenerator(id);
+  // const { nickname } = nicknameGenerator(id);
   
-  io.emit('nicknameList', users);
-  io.emit('message', messagesArray);
+  // io.emit('nicknameList', users);
+  // socket.emit('nicknameList', users);
+  // io.emit('message', messagesArray);
   // socket.broadcast.emit('systemMessage', loginNotification);
-  socket.emit('userName', nickname);
+  // socket.emit('userName', nickname);
   // socket.emit('systemMessage', `Você se conectou como ${nickname}`);
   
   createMessage(io, socket, id);
-  disconnect(io, socket, id);
-  nicknameManager(io, socket, id);
+  // disconnect(io, socket, id);
+  // nicknameManager(io, socket, id);
 });

fbedd4447e98dc35fbb0322bf23e0a7f40942484
Teika <shimworks.tk@gmail.com>
2022-01-19 19:04:20 -0300

    comenta funçao nao usada

--- controllers/serv.js
+++ controllers/serv.js
@@ -1,7 +1,7 @@
-const Msg = require('../models/servModel');
+// const Msg = require('../models/servModel');
 
-const newMsg = async (req, res) => {
-  const { chatMessage, nickname } = req.body;
-  await Msg.add(chatMessage, nickname);
-  res.render('success');
-};
+// const newMsg = async (req, res) => {
+//   const { chatMessage, nickname } = req.body;
+//   await Msg.add(chatMessage, nickname);
+//   res.render('success');
+// };

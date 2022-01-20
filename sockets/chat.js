b7c93b67e9658a5054ec6b75360b095395cd0bde
Guilherme Cesconeto <cesconeto@gmail.com>
2022-01-20 12:39:05 -0300

    Req 1 e 2 passando

--- sockets/chat.js
+++ sockets/chat.js
@@ -5,14 +5,20 @@ const onlineUsers = [];
 module.exports = (io) => {
     io.on('connection', (socket) => {
         // socket.emit('sendHistory', CHAT_HISTORY);
-        const defaultNick = socket.id.substring(0, 16);
-        onlineUsers.push({ id: socket.id, nickname: defaultNick });
-        socket.emit('changeNick', defaultNick);
+        // const defaultNick = socket.id.substring(0, 16);
+        // onlineUsers.push({ id: socket.id, nickname: defaultNick });
+        // socket.emit('changeNick', defaultNick);
 
+        console.log(socket.id);
+
+        socket.on('newUser', ({ id, nickname }) => {
+            onlineUsers.push({ id, nickname });
+        });
+        
         socket.on('message', ({ chatMessage, nickname }) => {
             io.emit('message', `${getDate()} - ${nickname} ${chatMessage}`);
         });
-
+        
         socket.on('newNick', ({ newNick }) => {
             const user = onlineUsers.find((selUser) => selUser.id === socket.id);
             user.nickname = newNick;

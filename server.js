4e4e3665776b2071c22917cd40e778e189a4d903
mroenca40 <mproenca.marketing@gmail.com>
2022-01-19 22:24:17 -0300

    req 01 criando conexão simultanea

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 require('dotenv').config();
+const cors = require('cors');
+const moment = require('moment');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.render('client');
-});
 
 const io = require('socket.io')(server, {
   cros: {
@@ -20,8 +18,25 @@ const io = require('socket.io')(server, {
   },
 });
 
-io.on('connection', () => {
-  console.log('feita a conexão! Novo usuario conectado.');
+io.on('connection', (socket) => {
+  console.log(`Feita a conexão! Novo usuario conectado. ${socket.id}`);
+
+  socket.on('nickname', (name) => { // escuta jonas 
+    io.emit('serverNickname', { nickname: name }); // envia jonas para front
+  });
+
+  socket.on('message', ({ nickname, chatMessage }) => {
+    // mensagem que vem do front o msg
+    const timestamp = moment().format('DD-MM-yyyy HH:mm:ss A');
+    
+    io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`); // ele envia a msg para o front
+  }); 
 });
 
-app.listen(PORT, () => console.log(`Ouvindo Socket.io server na porta ${PORT}!`));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.render('client');
+});
+
+server.listen(PORT, () => console.log(`Ouvindo Socket.io server na porta ${PORT}!`));

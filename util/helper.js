493e12d046d58eebf20d90acd01db185bced508e
Milton Castro <castro.milton07@gmail.com>
2022-01-20 21:44:41 -0300

    Requisito 2

--- util/helper.js
+++ util/helper.js
@@ -1,6 +1,6 @@
 const getTimeNow = () => {
   const dateAndTime = new Date();
-  const date = dateAndTime.toLocaleDateString();
+  const date = dateAndTime.toLocaleDateString().replaceAll('/', '-');
   const time = dateAndTime.toLocaleTimeString('en-US');
   return `${date} ${time}`;
 };

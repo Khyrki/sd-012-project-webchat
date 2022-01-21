3d56b53c9d5493c5a7252fee71edff6323ceb704
Martin <escorpmartin97@gmail.com>
2022-01-20 23:58:44 -0300

    retorna para el usuario a data e message

--- sockets/chat/index.js
+++ sockets/chat/index.js
@@ -1,11 +1,11 @@
-// const date = require('../../utils/createData');
+const date = require('../../utils/createData');
 
 const chat = (io) => {
   io.on('connection', (socket) => {
     console.log(`usuario conectado, id: ${socket.id}`);
     socket.emit('welcome', 'Seja benvindo ao chat!');
     socket.on('message', ({ chatMessage, nickname }) => {
-      io.emit('message', `${new Date()} - ${nickname}: ${chatMessage})}`);
+      io.emit('message', `${date(new Date())} - ${nickname}: ${chatMessage})}`);
     });
   });
 };

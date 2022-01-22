fc46dba2254e99dad6968a19ae7d63c45bd2feab
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-01-22 18:34:02 -0300

    Iniciando o projeto #VQV

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.json());
+
+const PORT = 3000;
+
+const socketIoServer = require('http').createServer(app);
+const io = require('socket.io')(socketIoServer, {
+  cors: { // Aqui existe um objeto de configuração, essas options são necessárias a partir da major 3 do socket.io 
+    origin: `http://localhost:${PORT}`, // origem permitida
+    methods: ['GET', 'POST'], // métodos permitidos
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Uma nova conexão com ${socket.id} foi estabelecida!`);
+});
+
+app.get('/', (_req, res) => {
+  res.render('index');
+});
+
+socketIoServer.listen(
+  PORT, console.log(`Socket.io Server listening on port ${PORT}!`),
+);

f5130f112999036486ae742a55b9047bbc3afc05
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-01-23 15:34:47 -0400

    Create index.js

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const rescue = require('express-rescue');
+const model = require('../models');
+
+module.exports.getAllMessages = rescue(async (_req, res, _next) => {
+  const messages = await model.getAllMessages();
+  const formatMessages = messages.map(({ chatMessage, nickname, timestamp }) => (
+    `${timestamp} - ${nickname}: ${chatMessage}`
+  ));
+  return res.status(200).render('webchat/index', { formatMessages });
+});

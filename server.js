2cbb35f7cca96ea88672f449ef085aefa9905167
Thalles Carneiro <thallescarneiro@gmail.com>
2022-01-23 09:09:23 -0300

    Iniciando o projeto. VQV! :rocket:

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqui
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+const onConnection = (_socket) => { };
+
+io.on('connection', onConnection);
+
+httpServer.listen(PORT, () => { console.log(`Listening on port: ${PORT}`); });

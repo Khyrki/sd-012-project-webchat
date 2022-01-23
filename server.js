ee1b8c2ef6cee7025173ec2f2ef3b1fc389c535e
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-01-23 15:35:12 -0400

    create server

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: `https://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+require('./sockets/webchat')(io);
+const controllers = require('./controllers');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/views', express.static(path.join(__dirname, 'views')));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', controllers.getAllMessages);
+
+httpServer.listen(PORT, () => console.log(`Socket.io server is listening on PORT ${PORT}`));

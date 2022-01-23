b056d6e379532c26af516165bd39dfdf8ad67263
José Carlos <kalarruda@hotmail.com>
2022-01-23 09:58:05 -0300

    resolvendo erros lint requsito 3

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,4 +1,4 @@
-const messageModel = require('../models/messagesModel')
+const messageModel = require('../models/messagesModel');
 const stringRandom = require('../helpers/stringGenerator');
 
 const STATUS_OK = 200;
@@ -7,7 +7,6 @@ const getAllMessages = async (req, res) => {
   try {
     const messages = await messageModel.getAll();
     return res.status(STATUS_OK).render('chat', { messages, nickname: stringRandom(16) });
-
   } catch (e) {
     console.log(e.message);
   }

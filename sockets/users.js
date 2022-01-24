0a9e625d81f0ee2f6f6da6327ad0d6eec30efc66
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2022-01-24 13:49:56 -0300

    Correções para o funcionamento do requisito 2

--- sockets/users.js
+++ sockets/users.js
@@ -1,9 +1,26 @@
+const faker = require('faker');
 const userModel = require('../models/userModel');
 const messageModel = require('../models/messageModel');
 
+function generateName() {
+  let fullName;
+  while (!/^[\w'-]{16}$/.test(fullName)) {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    fullName = `${firstName} ${lastName}`
+      .replace(' ', '-');
+  }
+  return fullName;
+}
+
 function client(socket) {
+  const fullName = generateName();
+  socket.broadcast.emit('newUser', fullName);
+  socket.emit('newUser', fullName);
+
   socket.on('user', async (nickname) => {
     socket.broadcast.emit('newUser', nickname);
+    socket.emit('newUser', nickname);
     // await userModel.create(socket.id, nickname);
   });
 }

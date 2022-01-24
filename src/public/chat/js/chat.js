3ac770bc44eb31c2b07cf6882672078b075b7b7e
Elias Forte <eliasforte.dev@gmail.com>
2022-01-23 23:39:17 -0300

    requisito 4

--- src/public/chat/js/chat.js
+++ src/public/chat/js/chat.js
@@ -6,6 +6,8 @@ const nicknameTilte = document.querySelector('.nickname');
 const formMessage = document.querySelector('.message-form');
 const inputMessage = document.querySelector('.message-input');
 const messageList = document.querySelector('.messages-list');
+const listUsers = document.querySelector('.users-list');
+const myUser = document.querySelector('.my-user');
 
 let nickname = '';
 
@@ -31,6 +33,7 @@ socket.on('message', (message) => {
 socket.on('randonNick', (nick) => {
   nicknameTilte.innerText = nick;
   nickname = nick;
+  myUser.innerText = nick;
 });
 
 formNickname.addEventListener('submit', (event) => {
@@ -38,7 +41,29 @@ formNickname.addEventListener('submit', (event) => {
   const newNick = nicknameInput.value;
   nicknameTilte.innerText = newNick;
   nickname = newNick;
+  myUser.innerText = newNick;
   socket.emit('changeNick', newNick);
   nicknameInput.value = '';
   return false;
 });
+
+const createUserLi = (arrayUsers) => {
+  arrayUsers.forEach((user) => {
+    if (user.id !== socket.id) {
+      const li = document.createElement('li');
+      li.innerText = user.nickname;
+      li.setAttribute('data-testid', 'online-user');
+      listUsers.appendChild(li);
+    }
+  });  
+};
+
+socket.on('connection', (arrayUsers) => {
+  console.log(arrayUsers);
+  listUsers.innerHTML = '';
+  createUserLi(arrayUsers);
+});
+
+window.onbeforeunload = () => {
+  socket.disconnect();
+};
--- src/sockets/chat.js
+++ src/sockets/chat.js
@@ -1,13 +1,50 @@
 const moment = require('moment');
 const { createMessages } = require('../models');
 
+const usersOnline = [];
+
+const messageSocket = (socket, io) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const timestamp = moment().format('DD-MM-YYYY HH:mm:ss A');
+    const userMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
+
+    io.emit('message', userMessage);
+
+    createMessages(chatMessage, nickname, timestamp);
+  });
+};
+
+const changeNickSocket = (socket, io) => {
+  socket.on('changeNick', (newNickname) => {
+    usersOnline.forEach((user, index) => {
+      if (user.id === socket.id) {
+        usersOnline[index].nickname = newNickname;
+      }
+    });
+
+    io.emit('connection', usersOnline);
+  });
+};
+
+const disconnectSocket = (socket, io) => {
+  socket.on('disconnect', () => {
+    usersOnline.forEach((user, index) => {
+      if (user.id === socket.id) {
+        usersOnline.splice(index, 1);
+      } 
+    });
+
+    io.emit('connection', usersOnline);
+  });
+};
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    io.emit('connection', 'Bem-vindo ao chat da Uol');
+    io.emit('welcome', 'Bem-vindo ao chat da Uol');
     const newNick = socket.id.substring(0, 16);
-
+    usersOnline.push({ nickname: newNick, id: socket.id });
+    io.emit('connection', usersOnline);
     socket.emit('randonNick', newNick);
-
     socket.on('nickname', (nickname) => {
       socket.emit('newNickname', nickname);
     });
@@ -16,13 +53,8 @@ module.exports = (io) => {
       console.log(nick);
     });
 
-    socket.on('message', ({ chatMessage, nickname }) => {
-      const timestamp = moment().format('DD-MM-YYYY HH:mm:ss A');
-      const userMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
-
-      io.emit('message', userMessage);
-
-      createMessages(chatMessage, nickname, timestamp);
-    });
+    disconnectSocket(socket, io);
+    changeNickSocket(socket, io);
+    messageSocket(socket, io);
   });
 };

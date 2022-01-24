22253a1cf268a7cb6426e8f9cdea21285fb1e912
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2022-01-24 06:10:01 -0300

    Requisito 2

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,10 +1,40 @@
+const usersOnline = [];
+
+const newUser = (socket, io) => {
+  socket.on('new-user', (user) => {
+    usersOnline.push({ id: socket.id, nickname: user });
+    console.log(usersOnline);
+    io.emit('online', usersOnline);
+  });
+};
+
+const editUser = (socket, io) => {
+  socket.on('edit-user', (user) => {
+    console.log(user);
+    const indexUser = usersOnline.findIndex((item) => item.id === socket.id);
+    usersOnline[indexUser].nickname = user;
+    console.log(usersOnline);
+    io.emit('online', usersOnline);
+  });
+};
+
+const offlineUser = (socket, io) => {
+  socket.on('disconnect', () => {
+    console.log(`Um usuário desconectou de ${socket.id}`);
+    const indexUser = usersOnline.findIndex((item) => item.id === socket.id);
+    usersOnline.splice(indexUser, 1);
+    io.emit('online', usersOnline);
+  });
+};
+
 const chat = (io) => {
   io.on('connection', (socket) => {
-    console.log(`Um usuário conectou em ${socket.id}`);
-    socket.on('disconnect', () => {
-      console.log(`Um usuário desconectou em ${socket.id}`);
-    });
+    newUser(socket, io);
+    editUser(socket, io);
 
+    console.log(`Um usuário conectou em ${socket.id}`); 
+
+    offlineUser(socket, io);
     socket.on('message', (message) => {
       const dateMessage = new Date();
       const timeMessage = dateMessage.toLocaleTimeString();

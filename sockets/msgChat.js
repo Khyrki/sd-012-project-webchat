f218846db242c103f16bbb1a240ca3a05aaa95dc
aythan azevedo <aythan16@gmail.com>
2022-01-24 14:10:17 -0300

    rename

--- sockets/msgChat.js
+++ sockets/msgChat.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 const chat = require('../models/chat');
 
-const onlineUsers = [];
+const users = [];
 // agradeço ao kevin por ter tirado um tempo para me auxiliar nessa relação do socket com os event listener
 // estára referenciado no server.js
 module.exports = (io) => io.on('connection', (socket) => {
@@ -13,19 +13,19 @@ module.exports = (io) => io.on('connection', (socket) => {
   });
     // ao receber um impulso do eventListener adiciona o id ao array  emite uma chamada para  o event users no ejs
     socket.on('newUser', (nickname) => {
-      onlineUsers.push({ id: socket.id, nickname });
-      io.emit('onlineUsers', onlineUsers);
+      users.push({ id: socket.id, nickname });
+      io.emit('users', users);
     });
       // ao receber um impulso do eventListener atualiza o nickname pelo id e   emite uma chamada para  o event users no ejs
       socket.on('updateNick', (nickname) => {
-        const user = onlineUsers.find(({ id }) => id === socket.id);
+        const user = users.find(({ id }) => id === socket.id);
         user.nickname = nickname;
-        io.emit('onlineUsers', onlineUsers);
+        io.emit('users', users);
       });
   // remove o  usuario(id) da lista do array iterando o array e removendo ele pelo metodo splice que remove ou substitui o 
   // elemento de acordo com a localização que vocẽ indica
   socket.on('disconnect', () => {
-    const index = onlineUsers.findIndex(({ id }) => id === socket.id);
-    onlineUsers.splice(index, index + 1); io.emit('onlineUsers', onlineUsers);
+    const index = users.findIndex(({ id }) => id === socket.id);
+    users.splice(index, index + 1); io.emit('users', users);
   });
 });

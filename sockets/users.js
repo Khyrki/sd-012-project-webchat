ea188f0e8c90c59d48525130d6f33f8e4c5036c4
camila-mp <ca.malvp@gmail.com>
2022-01-25 12:01:53 -0300

    refatora o socket users.js

--- sockets/users.js
+++ sockets/users.js
@@ -1,25 +1,36 @@
 const randomNickname = require('../helpers/randomNickname');
 
-module.exports = (io) => {
-  let userList = [];
-  io.on('connection', (socket) => { 
-    socket.on('newUser', () => {
-      const nickname = randomNickname(16);
-      userList.push({ socketId: socket.id, nickname });
-      socket.emit('specificUserUpdate', ({ nickname, userList }));
-      socket.broadcast.emit('newUser', ({ nickname, userList }));
-    });
+let userList = [];
 
-    socket.on('nicknameChange', ({ newName }) => {
-      const userIndex = userList.findIndex((user) => user.socketId === socket.id);
-      userList[userIndex].nickname = newName;
-      io.emit('refreshUsers', (userList));
-    });
-    
-    socket.on('disconnect', () => {
-      const updatedUserList = userList.filter((user) => socket.id !== user.socketId);
-      userList = updatedUserList;
-      io.emit('userDisconnect', (userList));
-    });
+const handleNewUser = (socket) => {
+  socket.on('newUser', () => {
+    const nickname = randomNickname(16);
+    userList.push({ socketId: socket.id, nickname });
+    socket.emit('specificUserUpdate', ({ nickname, userList }));
+    socket.broadcast.emit('newUser', ({ nickname, userList }));
+  });
+};
+
+const handleNicknameChange = (socket, io) => {
+  socket.on('nicknameChange', ({ newName }) => {
+    const userIndex = userList.findIndex((user) => user.socketId === socket.id);
+    userList[userIndex].nickname = newName;
+    io.emit('refreshUsers', (userList));
+  });
+};
+
+const handleDisconnect = (socket, io) => {
+  socket.on('disconnect', () => {
+    const updatedUserList = userList.filter((user) => socket.id !== user.socketId);
+    userList = updatedUserList;
+    io.emit('userDisconnect', (userList));
+  });
+};
+
+module.exports = (io) => {
+  io.on('connection', (socket) => { 
+    handleNewUser(socket);
+    handleNicknameChange(socket, io);
+    handleDisconnect(socket, io);
   });
 };

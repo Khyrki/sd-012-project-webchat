8635fe519dd7c782fb67306ceed16660cfdb6ef3
walttoncoelho <walltton@outlook.com>
2022-01-25 08:14:32 -0300

    Criando o chat

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+const http = require('http').createServer(app);
+const moment = require('moment');
+
+const bodyParser = require('body-parser');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const date = moment().format('DD-MM-yyyy HH:mm:ss A');
+
+io.on('connection', (socket) => {
+  socket.on('message', (messagem) => {
+    io.emit('message', `${date} - ${messagem.nickname}: ${messagem.chatMessage}`);
+  });
+});
+
+app.get('/', (_req, res) => res.send('Webchat'));
+
+http.listen(PORT, () => console.log(`Iniciando na porta ${PORT}!`));

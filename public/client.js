2d573e1d16374953aac9e2cc57d7f110579dbff7
walttoncoelho <walltton@outlook.com>
2022-01-25 10:38:35 -0300

    Frontend chat

---
+++ public/client.js
@@ -0,0 +1,74 @@
+const socket = window.io(); // na aula 30.3 o renato explica o pq disso
+
+const usersList = document.querySelector('#users'); 
+const messagesList = document.querySelector('#messages'); 
+const nicknameInput = document.querySelector('#nickname'); 
+const messageInput = document.querySelector('#messageBox'); 
+const nicknameBtn = document.querySelector('#nicknameBtn'); 
+const sendBtn = document.querySelector('#sendBtn'); 
+
+const generateNickname = (length) => { // gera um nickname aleatório
+  let result = '';
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i += 1) {
+    result += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return result;
+};
+
+let nicknameGenerate = generateNickname(16); 
+
+socket.emit('newUser', nicknameGenerate); 
+
+const createMessage = (newMessage) => { 
+  const li = document.createElement('li');
+  li.innerText = newMessage;
+  li.setAttribute('data-testid', 'message');
+  messagesList.appendChild(li);
+};
+
+const userList = (user) => { 
+  const li = document.createElement('li');
+  li.innerText = user;
+  li.setAttribute('data-testid', 'online-user');
+  usersList.appendChild(li);
+};
+
+nicknameBtn.addEventListener('click', () => {
+  nicknameGenerate = nicknameInput.value; 
+  nicknameInput.value = ''; 
+  socket.emit('nickname', nicknameGenerate); 
+});
+
+sendBtn.addEventListener('click', () => { 
+  socket.emit('message', { 
+    nickname: nicknameGenerate,
+    chatMessage: messageInput.value, 
+  });
+ 
+  nicknameGenerate = '';
+  messageInput.value = '';
+  return false;
+});
+
+userList(nicknameGenerate);
+socket.on('message', (newMessage) => {
+  createMessage(newMessage); 
+});
+
+// socket de mensagens de user
+socket.on('messageLoad', (newMessage) => {
+  createMessage(newMessage); 
+});
+
+// socket de user
+socket.on('loadUsers', (users) => {
+  usersList.innerHTML = '';
+  userList(nicknameGenerate);
+  console.log(users);
+  users.forEach((user) => {
+    if (user !== nicknameGenerate) {
+      return userList(user);
+    }
+  });
+});
--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const express = require('express');
+const express = require('express'); 
 
 const app = express();
 const PORT = 3000;
 const http = require('http').createServer(app);
-const moment = require('moment');
+const moment = require('moment'); // https://momentjs.com/
 
 const bodyParser = require('body-parser');
 
-const io = require('socket.io')(http, {
+const io = require('socket.io')(http, { // https://socket.io/docs/
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`)); 
 
 const date = moment().format('DD-MM-yyyy HH:mm:ss A');
 
@@ -27,7 +25,5 @@ io.on('connection', (socket) => {
     io.emit('message', `${date} - ${messagem.nickname}: ${messagem.chatMessage}`);
   });
 });
-
-app.get('/', (_req, res) => res.send('Webchat'));
 
 http.listen(PORT, () => console.log(`Iniciando na porta ${PORT}!`));

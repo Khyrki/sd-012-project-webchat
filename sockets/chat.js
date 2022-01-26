75e2fd2470b7019a7fbfd936abb0acc99bc39c9e
juliabaptista <juliabaptistadeoliveira@gmail.com>
2022-01-26 10:30:20 -0300

    Req-01: edit message format in sockets/chat.js to get current time stamp

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,5 +1,7 @@
 const randomstring = require('randomstring');
 
+const currentTimeStamp = require('../helpers/currentTimeStamp');
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
     //   console.log('conectado'); shows that we have connection - users are connected!
@@ -7,8 +9,10 @@ module.exports = (io) => {
     socket.emit('nickname', randomstring.generate(16)); // emits nickname from the server to one socket connection
     
     socket.on('message', (msg) => {
-      console.log(`Mensagem: ${msg}`);
-      io.emit('message', msg);
+    // gets message from client and send it to all in right format
+      const timestamp = currentTimeStamp();
+      const clientMessage = `${timestamp} - ${msg.nickname}: ${msg.chatMessage}`;
+      io.emit('message', clientMessage);
     });
   });
 };

35416ee3745c72c29f8563dec3da80f0078b70ed
AndreHammel <hammel.engineer@gmail.com>
2022-01-26 20:54:38 -0300

    lint

--- teste.js
+++ teste.js
@@ -12,24 +12,3 @@
 // const nome = crypto.randomBytes(8).toString('hex');
 
 // console.log(nome);
-
-const a = [
-  {
-    _id: '61f1cd5bde0a4fda7177cf3f',
-    message: 'asdf',
-    nickname: 'arV0n9xSvx-AvIXB',
-    timestamp: '26-01-2022 7:38:19 PM',
-  },
-  {
-    _id: '61f1cd5fde0a4fda7177cf40',
-    message: 'fdfdfd',
-    nickname: 'dY3O8G4GX3fA3imb',
-    timestamp: '26-01-2022 7:38:43 PM',
-  },
-  {
-    _id: '61f1cd62de0a4fda7177cf41',
-    message: 'asdf',
-    nickname: 'd_N_SPBiaEE2rdXy',
-    timestamp: '26-01-2022 7:38:26 PM',
-  },
-];

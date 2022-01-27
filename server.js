7dcac74132635ca992ad2c402f29338da3a1071b
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-01-27 04:25:31 -0300

    fix(server): corrige path e url

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const PORT = 3000;
 
 const io = require('socket.io')(http, {
   cors: {
-    origin: `http://localhost:${3000}`,
+    origin: `http://localhost:${PORT}`,
     methods: ['GET', 'POST'],
   },
 });
 
 require('./sockets/webChat')(io);
 
-app.use(express.static(`${__dirname} + /public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname} + /public`);
+  res.sendFile(`${__dirname}/public`);
 });
 
 http.listen(PORT, () => {

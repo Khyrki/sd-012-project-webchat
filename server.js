b660225c1e4d2c39785be90dd35945c89a318a1f
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-01-27 01:57:18 -0300

    feat: adiciona estrutura inicial do server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const PORT = 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${3000}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+require('./sockets/webChat')(io);
+
+app.use(express.static(`${__dirname} + /public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname} + /public`);
+});
+
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});

fd4d88159399c60c5245950afcdcc4ee3b48f55a
Uriel Bochi <urielbochi@gmail.com>
2022-01-26 23:12:52 -0300

    server nao foi salvo

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const io = require('socket.io')(server, {
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-require('./pastuxa/chatSocket')(io);
+require('./chatSocket')(io);
 
 app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));

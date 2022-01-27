ef2601bff4b1c4630c296a895ed1176d24555591
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-01-27 12:04:25 -0300

    feat(server): adiciona criacao de nickname aleatorio e evento

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,46 @@
 const socket = window.io();
 
-const form = document.querySelector('form');
-const input = document.querySelector('#input');
+const messagesForm = document.querySelector('#messages-form');
+const messagesInput = document.querySelector('#messages-input');
 
-form.addEventListener('submit', (e) => {
+const nicknameForm = document.querySelector('#nickname-form');
+const nicknameInput = document.querySelector('#nickname-box');
+const showNickname = document.querySelector('#online-user');
+
+function createRandomNickname() {
+  let randomNickname = '';
+  const size = 16;
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let index = 0; index < size; index += 1) {
+    randomNickname += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return randomNickname;
+}
+// referencia: https://www.webtutorial.com.br/funcao-para-gerar-uma-string-aleatoria-random-com-caracteres-especificos-em-javascript/
+showNickname.innerHTML = sessionStorage.getItem('nickname') || createRandomNickname();
+
+nicknameForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('message', input.value);
-  input.value = '';
+  const newNickname = nicknameInput.value;
+  nicknameInput.value = '';
+
+  sessionStorage.setItem('nickname', newNickname);
+  showNickname.innerText = newNickname;
+});
+
+messagesForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('message', messagesInput.value);
+  messagesInput.value = '';
   return false;
 });
 
-const createMessage = (message) => {
+const createMessageList = (message) => {
   const ul = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = message;
+  li.setAttribute('data-testid', 'message');
   ul.appendChild(li);
 };
 
-socket.on('message', (message) => createMessage(message));
+socket.on('message', (message) => createMessageList(message));

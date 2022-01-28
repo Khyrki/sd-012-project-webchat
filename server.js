606640d37a75fe37890b7506413b786f992949cf
rafaelprimon <rafaelfernandesprimon@gmail.com>
2022-01-28 16:57:48 -0300

    implementa configurações inicias

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-// Faça seu código aqui
+const express = require('express');
+
+const moment = require('moment');
+
+const app = express();
+const http = require('http').createServer(app);
+
+app.use(express.json());
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'], 
+  } });
+
+const PORT = 3000;
+
+http.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`)); 

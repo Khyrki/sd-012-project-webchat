af2a1fffb9be3a8fca8ad1728f546626ff180ff7
Prih Scapin <prih.scapin@gmail.com>
2022-01-28 19:15:31 -0300

    Req 01

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const moment = require('moment');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const io = require('socket.io')(http, {
+    cors: {
+      origin: 'http://localhost:3000', 
+      methods: ['GET', 'POST'],
+    } });
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const date = moment().format('DD-MM-yyyy HH:mm:ss A');
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    io.emit('message', `${chatMessage}, ${nickname}, ${date}`);
+    });
+});
+
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, './public/chat.html'));
+});   
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});

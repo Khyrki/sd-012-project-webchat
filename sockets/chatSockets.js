8658d6f09d368edc3771f5c933fa1f66f24e7710
Fabio Ryudi <ryu_fabio@hotmail.com>
2022-01-28 16:23:14 -0300

    sim, apaguei tudo pra refatorar

--- sockets/chatSockets.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment');
-
-module.exports = (io) => io.on('connection', (socket) => {
-  socket.on('message', ({ chatMessage, nickname }) => {
-    const dateByMoment = moment().format('DD-MM-YYYY HH:mm:ss');
-    io.emit('message', `${dateByMoment} ${nickname}: ${chatMessage}`);
-  }); 
-});

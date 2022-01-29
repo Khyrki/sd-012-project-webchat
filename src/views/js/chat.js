6fc98f5a74177b3964e3d087fc18d261559106e7
Bruno Gomes <brunognovaes.cm@gmail.com>
2022-01-28 22:52:13 -0300

    random nickname

--- src/views/js/chat.js
+++ src/views/js/chat.js
@@ -6,6 +6,24 @@ const messageForm = document.querySelector('#messageForm');
 const messageInput = document.querySelector('#messageInput');
 const messages = document.querySelector('#messages');
 
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const generateString = (length) => {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i += 1) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+};
+
+// Gerador de id - https://www.programiz.com/javascript/examples/generate-random-strings
+
+if (!sessionStorage.getItem('nickname')) {
+  const nickname = generateString(16);
+  sessionStorage.setItem('nickname', nickname);
+}
+
 messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const nickname = sessionStorage.getItem('nickname');

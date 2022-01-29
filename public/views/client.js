aa94dbe6228f1c45c4034b0f46dc664ac8a59921
Wadson92 <wadson.dev92@gmail.com>
2022-01-29 15:41:57 -0300

    Resolve requisito 4

--- public/views/client.js
+++ public/views/client.js
@@ -4,7 +4,10 @@ const form = document.querySelector('.form');
 const inputMessage = document.querySelector('.message-input');
 const nameUser = document.querySelector('.nickname-input');
 const messagesUl = document.querySelector('.messages');
-const tokenUser = document.querySelector('.token-user');
+const ulUsers = document.querySelector('.users');
+const buttonNickname = document.querySelector('.nickname-btn');
+
+let tokenUser = '';
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -21,7 +24,7 @@ const createMessage = (message) => {
 };
 
 const createToken = (token) => {
-  tokenUser.innerText = token;
+  tokenUser = token;
 };
 
 const getAll = (data) => {
@@ -30,6 +33,29 @@ const getAll = (data) => {
   });
 };
 
+const newUser = (data) => {
+  ulUsers.innerHTML = '';
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'online-user');
+  li.innerText = tokenUser;
+  ulUsers.appendChild(li);
+
+  data.forEach((users) => {
+    if (tokenUser === users.nickname) return;
+    const othersUsers = document.createElement('li');
+    othersUsers.setAttribute('data-testid', 'online-user');
+    othersUsers.innerText = users.nickname;
+    ulUsers.appendChild(othersUsers);
+  });
+};
+
+buttonNickname.addEventListener('click', (event) => {
+  event.preventDefault();
+  tokenUser = nameUser.value;
+  socket.emit('newNickname', tokenUser);
+});
+
 socket.on('message', (message) => createMessage(message));
 socket.on('token', (token) => createToken(token));
 socket.on('allData', (data) => getAll(data));
+socket.on('newUser', (data) => newUser(data));
--- sockets/chat.js
+++ sockets/chat.js
@@ -2,6 +2,8 @@ const { format } = require('date-fns');
 const create = require('../models/create');
 const getAll = require('../models/get');
 
+let newUser = [];
+
 const createMessage = (socket, io) => {
   const { id } = socket;
   const timestamp = format(new Date(), 'dd-MM-yyyy HH:mm:ss');
@@ -14,8 +16,13 @@ const createMessage = (socket, io) => {
 const createToken = (socket) => {
   const nickname = [...Array(16)]
     .map((_i) => Math.floor(Math.random() * 36).toString(36))
-    .join(''); 
+    .join('');
+  newUser.push({ nickname, id: socket.id });
   socket.emit('token', nickname);
+};
+
+const userLog = (io) => {
+  io.emit('newUser', newUser);
 };
 
 const findAll = async (socket) => {
@@ -23,9 +30,31 @@ const findAll = async (socket) => {
   socket.emit('allData', allData);
 };
 
+const newNickname = (socket, io) => {
+  socket.on('newNickname', (nickname) => {
+    newUser.forEach((user) => {
+      const findUser = user;
+      if (findUser.id === socket.id) {
+        findUser.nickname = nickname;
+      }
+    });
+    io.emit('newUser', newUser);
+  });
+};
+
+const userLogout = (socket, io) => {
+  socket.on('disconnect', () => {
+    newUser = newUser.filter((user) => user.id !== socket.id);
+    io.emit('newUser', newUser);
+  });
+};
+
 const messages = (io) => io.on('connection', (socket) => {
   createMessage(socket, io);
   createToken(socket);
   findAll(socket);
+  userLog(io);
+  newNickname(socket, io);
+  userLogout(socket, io);
 });
 module.exports = messages;

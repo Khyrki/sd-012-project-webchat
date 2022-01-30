7f42ba479abf804cdb97811f862e782be54978c8
Guilherme Dias <gmd.delta@gmail.com>
2022-01-30 05:40:08 -0300

    [PT-BR]Resolvido requisito 2. [EN] Solved 2nd requirement.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,35 @@
 const socket = window.io();
 
-console.log('oi');
+const nickname = document.querySelector('.nickname');
+const nicknameForm = document.querySelector('.nicknameForm');
+const nicknameInput = document.querySelector('#nicknameInput');
 
-const form = document.querySelector('form');
+const messageForm = document.querySelector('.messageForm');
 const inputMessage = document.querySelector('#messageInput');
-form.addEventListener('submit', (e) => {
+
+nicknameForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('message', inputMessage.value);
+  console.log('entrou aqui');
+  nickname.innerHTML = nicknameInput.value;
+  nicknameInput.value = '';
+});
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('message', { nickname: nickname.innerHTML, chatMessage: inputMessage.value });
   inputMessage.value = '';
-  return false;
 });
 
 const createMessage = (message) => {
   const messagesUl = document.querySelector('.messages');
   const li = document.createElement('li');
+  li.dataset.testid = 'message';
   li.innerText = message;
   messagesUl.appendChild(li);
 };
 
 socket.on('message', (message) => createMessage(message));
+
+window.onload = async () => {
+  await socket.on('connect', () => { nickname.innerHTML = socket.id.slice(0, 16); });
+};

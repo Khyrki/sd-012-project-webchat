444fa5315e42001de7e8b103af66b82498110fb3
Adryan Rosa <adryan.lmrosa@gmail.com>
2022-01-30 17:33:11 -0300

    Inicia server.js com evento socket message

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const date = new Date().toLocaleString().split(',').join(' ')
+      .replaceAll('/', '-');
+    const message = `${date} - ${nickname} ${chatMessage}`;
+
+    io.emit('message', message);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello there');
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});

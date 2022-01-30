314f16278f7f011ccc7cc6ceb0c0410f07802d4c
CamilaDamasio <camiladamasioef@hotmail.com>
2022-01-30 15:03:42 -0300

    Requisito 4

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,12 +10,12 @@ const nicknameBtn = document.querySelector('#nickname-button');
 nicknameBtn.addEventListener('click', (e) => {
   e.preventDefault();
   nickText.innerHTML = nickname.value;
+  socket.emit('nicknameList', nickname.value);
   nickname.value = '';
 });
 
 socket.on('nickname', (name) => {
   nickText.innerText = name;
-  nickText.setAttribute('data-testid', 'online-user');
 });
 
 formMsg.addEventListener('submit', (e) => {
@@ -35,3 +35,29 @@ const createMessage = (message) => {
 };
 
 socket.on('message', (chatMessage) => createMessage(chatMessage));
+
+const ulUserList = document.querySelector('.online-list');
+socket.on('usuariosConectados', (usersList) => {
+  console.log('test', usersList);
+  ulUserList.innerHTML = '';
+
+  const user = usersList.find(({ id }) => id === socket.id); // guarda o usuário da página atual para colocá-lo no topo da lista de usuários mais adiante.
+
+// console.log('socket.id de cada cliente >', socket.id);
+
+const userFilter = usersList.filter(({ id }) => id !== socket.id); // guarda a lista de todos os usuários connectados, exceto ele mesmo.
+// Obs.: gera um novo array.
+
+userFilter.unshift(user); // coloca o usuário atual no topo da lista 
+
+userFilter.forEach((userL) => {
+    const li = document.createElement('li');
+    li.innerText = userL.nickname;
+    li.setAttribute('data-testid', 'online-user');
+    ulUserList.appendChild(li);
+  });
+});
+
+socket.on('newName', (newName) => {
+  nickText.innerText = newName;
+});
--- sockets/chat.js
+++ sockets/chat.js
@@ -7,17 +7,43 @@ const randomName = () => {
   return random.substring(0, 16);
 };
 
-module.exports = (io) => io.on('connection', (socket) => {
-  const randomNickname = randomName();
-  socket.emit('nickname', randomNickname);
+let usersList = [];
 
+const messageSocket = (socket, io) => {
   socket.on('message', ({ chatMessage, nickname }) => {
     const date = new Date();
     const dateMsg = `${date.getDate()}-0${date.getMonth() + 1}-${date
-      .getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date
-      .getSeconds()}`;
-    io.emit('message',
-    `${dateMsg} - ${nickname}: ${chatMessage}`);
+    .getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date
+    .getSeconds()}`;
+    io.emit('message', `${dateMsg} - ${nickname}: ${chatMessage}`);
     createMsgHistory(chatMessage, nickname, dateMsg);
   });
-});
+};
+
+// Requisito 4 feito com a ajuda do colega Cristiano Lima e esclarecido com a ajuda do colega Bruno Augusto.
+const disconnect = (socket, io) => {
+  socket.on('disconnect', () => {
+    usersList = usersList.filter((item) => item.id !== socket.id);
+    io.emit('usuariosConectados', usersList);
+  });
+};
+    
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    const randomNickname = `${randomName()}`;
+    usersList.push({ id: socket.id, nickname: randomNickname });
+    socket.emit('nickname', randomNickname);
+    io.emit('usuariosConectados', usersList);
+    socket.on('nicknameList', (nickName) => {
+    usersList.forEach((user, index) => {
+      if (user.id === socket.id) {
+        usersList[index].nickname = nickName;
+      }
+    });
+    io.emit('usuariosConectados', usersList);
+    socket.emit('newName', nickName);
+  });
+  messageSocket(socket, io);
+  disconnect(socket, io);
+  });
+};

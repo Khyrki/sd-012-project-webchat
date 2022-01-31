cc17c673d58e2b54952f32282d135e0bfe43be76
Alan Gonçalves <vekio.soft@gmail.com>
2022-01-30 21:02:24 -0300

    refactoring req1 - config socket in server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 // Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log(`Usuário conectado. ID: ${socket.id} `);
+});
+
+app.use(express.static(`${__dirname} + /public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname} + /src/public/index.html`);
+});
+
+http.listen(3000, () => {
+  console.log('server ouvindo na porta 3000');
+});

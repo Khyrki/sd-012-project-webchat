5dc49a080e00c6c24b75aee6d132617d6bcfb239
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2022-01-31 10:28:13 -0300

    setup inicial do webchat

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', () => {
+  console.log('Alguém entrou');
+});
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, './public/index.html'));
+});
+
+httpServer.listen(process.env.PORT, () => console.log(`Conectado na porta ${process.env.PORT}`));

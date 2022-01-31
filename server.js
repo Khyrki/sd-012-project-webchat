dab6f0869a73f9dc3c1e57d636ed8d1551d94ffa
Alan Gonçalves <vekio.soft@gmail.com>
2022-01-31 13:22:06 -0300

    requisito 2

--- server.js
+++ server.js
@@ -11,10 +11,25 @@ const io = require('socket.io')(http, { // servidor ao qual iremos nos comunicar
   },
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
+  const allSockets = await io.allSockets();
+
+  io.sockets.emit('userJoined', [...allSockets]);
+
+  socket.on('changedNickname', ({ oldNick, newNick }) => {
+    console.log(socket.id);
+  });
+
+  socket.on('disconnect', async () => {
+    const sockets = await io.allSockets();
+
+    io.sockets.emit('userLeft', [...sockets]);
+    socket.disconnect();
+  });
+
   socket.on('message', ({ chatMessage, nickname }) => {
     const timestamp = moment().format('DD-MM-YYYY HH:mm');
-    io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
+    io.sockets.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
   });
 });
 
@@ -25,7 +40,7 @@ app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/view`); // aqui informamos onde as views serão procuradas
 
 app.get('/', (_req, res) => {
-  res.render(`${__dirname}/src/views/index`, { teste: 'hehe' });
+  res.render(`${__dirname}/src/views/index`);
 });
 
 http.listen(PORT, () => console.log(`App runnign on port: ${PORT}`));

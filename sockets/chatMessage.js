8f3babab081424ff2676aa96f26edcfb9aadbcf8
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2022-01-31 11:37:33 -0300

    corrige para lançar a data somente no backend

--- sockets/chatMessage.js
+++ sockets/chatMessage.js
@@ -3,8 +3,7 @@ const formatDate = require('./helpers/formatDate');
 module.exports = (io) => {
   io.on('connection', (socket) => {
     socket.on('message', (msg) => {
-      const formatedDate = formatDate(msg.date);
-      io.emit('message', `${formatedDate} - ${msg.nickname}: ${msg.chatMessage}`);
+      io.emit('message', `${formatDate()} - ${msg.nickname}: ${msg.chatMessage}`);
     });
   });
 };
--- sockets/helpers/formatDate.js
+++ sockets/helpers/formatDate.js
@@ -1,16 +1,19 @@
-module.exports = (date) => {
-  const d = new Date(date);
-      let month = `${d.getMonth() + 1}`;
-      let day = `${d.getDate()}`;
-      const year = d.getFullYear();
-      const hour = d.getHours();
-      let minutes = d.getMinutes();
-      let seconds = d.getSeconds();
+// Boa parte dessa função de formatação foi feita com base nessa thread do
+// Stackoverflow: https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-dd
+
+module.exports = () => {
+  const date = new Date(Date.now());
+  let month = `${date.getMonth() + 1}`;
+  let day = `${date.getDate()}`;
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
   if (month.length < 2) { month = `0${month}`; }
   if (day.length < 2) { day = `0${day}`; }
   if (minutes.length < 2) { minutes = `0${minutes}`; }
   if (seconds.length < 2) { seconds = `0${seconds}`; }
 
-  return `${[year, month, day].join('-')} ${hour}:${minutes}:${seconds}`;
+  return `${[day, month, year].join('-')} ${hour}:${minutes}:${seconds}`;
 };

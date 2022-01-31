9441912fbe267f267f11f8e8b38a8258018438f2
Andre Jaques <andrejaques.b@gmail.com>
2022-01-31 14:36:34 -0300

    add ejs and css in views and fixed lint

--- server.js
+++ server.js
@@ -3,24 +3,27 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const cors = require('cors');
 const io = require('socket.io')(http, {
-	cors: {
-		origin: 'http://localhost:3000',
-		method: ['GET', 'POST'],
-	},
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
 });
 
 io.on('connection', (socket) => {
-	console.log('Alguém se conectou!');
+  socket.emit('entered', 'Bem vindo');
+  console.log('Alguém se conectou!');
 });
 
 const { PORT } = process.env;
 
 app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
 
 app.get('/', (_req, res) => {
-	res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/src/views/index.html`);
 });
 
 http.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

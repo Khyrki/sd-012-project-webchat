f4e4c208943758ab55853d90d44c88f47cdedaff
Tulio <tuliotrindade99@gmail.com>
2022-01-31 15:57:09 -0300

    vá commit imediatamente

--- public/js/chat.js
+++ public/js/chat.js
@@ -57,7 +57,7 @@ const clearUsers = () => {
   }
 };
 
-/* https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another */
+/* https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another  */
 
 function arrUsersMove(arr, from, to) {
   arr.splice(to, 0, arr.splice(from, 1)[0]);
